08e9d9ffc83bc5a027551b734cef68e9e75134cb
kzxc2524 <kzxc2524@naver.com>
2020-01-14 11:13:16 +0900

    form hook onChange

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
 import './App.css';
 import SignUp from './component/SignUp.jsx';
 
-function App() {
-  return (
-    <div>
-      <h1>hi</h1>
-      <SignUp/>
-    </div>
+
+
+const App = () => {
+
+  const [formValue, setformValues] = useState({
+    name:'',
+    userId:'',
+    userPw:'',
+    userPwAgain:''
+  });
+
+  const formValueChange = (e) =>{
+  console.log(e.target)
+    setformValues({
+      ...formValue,
+      [e.target.name] : e.target.value
+    });
+  }
+
+  useEffect(() => {
+    console.log(formValue);
+  }, [formValue]);
+
+
+  return(
+    <SignUp formValue={formValue} formValueChange={formValueChange} />
   );
 }
 
--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
 
-const SignUp = () => {
+const SignUp = ({ formValue, formValueChange }) => {
     return(
         <form>
             <div>
                 <label htmlFor="userNm"> {/*for 대신 htmlFor */}
                     <span>이름</span>
-                    <input type="text" id="userNm" value="" />
+                    <input type="text" id="userNm" name="name" value={formValue.name} onChange={formValueChange} placeholder="이름을 적어주세요" />
                 </label>
                 <label htmlFor="userId">
                     <span>아이디</span>
-                    <input type="text" id="userId" value="" />
+                    <input type="text" id="userId" name="userId" value={formValue.userId} onChange={formValueChange} />
                 </label>
             </div>
             <div>
                 <label htmlFor="userPw">
                     <span>비밀번호</span>
-                    <input type="password" id="userPw" value="" />
+                    <input type="password" id="userPw" name="userPw" value={formValue.userPw} onChange={formValueChange} />
                 </label>
                 <label htmlFor="userPwAgain">
                     <span>비밀번호 확인</span>
-                    <input type="password" id="userPwAgain" value="" />
+                    <input type="password" id="userPwAgain" name="userPwAgain" value={formValue.userPwAgain} onChange={formValueChange} />
                 </label>
             </div>
         </form>
